9d0e2ee6bb67d5cd0bba90e009fb70a08789a65d
Zhang1933 <193319076@qq.com>
2021-07-30 20:13:58 +0800

    分为了两个配置，不保证两个配置文件兼容

---
+++ nvim/general/templates/head.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<string>
+#include<math.h>
+#include<cstring>
+#include<queue>
+#include<set>
+#include<stack>
+using namespace std;
+#define debug(x) cout<<"###"<<x<<"###"<<endl;
+typedef long long ll;
+const double eps=1e-8;
+const int INF=0x3f3f3f3f;
+int main(){
+}
